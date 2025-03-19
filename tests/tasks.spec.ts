821f93c8de55fd9df9cfa86da7522b79eb784fa7
Joao Lopes <jgsantanalopes@gmail.com>
2025-03-19 15:34:58 -0300

    test: adding visible text verification in the testcase

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,15 +1,21 @@
 import {test, expect} from '@playwright/test'
-import { faker } from '@faker-js/faker'
 
-test('deve poder cadastrar uma nova tarefa', async ( {page} ) => {
+test('deve poder cadastrar uma nova tarefa', async ( {page, request} ) => {
+
+    const taskName = 'Ler um livro de Typescript'
+
+    await request.delete('http://localhost:3333/helper/tasks/' + taskName)
 
     await page.goto('http://localhost:8080')
 
     const inputTaskName = page.locator('input[class*=InputNewTask]')
-    await inputTaskName.fill(faker.lorem.words())
+    await inputTaskName.fill(taskName)
     //await inputTaskName.press('Enter') Se quizesse cadastrar a tarefa usando a tecla Enter
     //await page.click('xpath=//button[contains(text(), "Create")]') // outra estrategia com xpath
     await page.click('css=button >> text=Create')
+
+    const target = page.locator(`css=.task-item p >> text=${taskName}`)
+    await expect(target).toBeVisible()
 
 })
 
