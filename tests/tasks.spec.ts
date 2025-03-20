bee54561181c9175ccfac0574eb7ef1ee12a4bb4
Joao Lopes <jgsantanalopes@gmail.com>
2025-03-20 10:30:33 -0300

    test: should not allow creation of duplicate task

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,6 +1,6 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-test('deve poder cadastrar uma nova tarefa', async ( {page, request} ) => {
+test('deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
 
     const taskName = 'Ler um livro de Typescript'
 
@@ -10,12 +10,34 @@ test('deve poder cadastrar uma nova tarefa', async ( {page, request} ) => {
 
     const inputTaskName = page.locator('input[class*=InputNewTask]')
     await inputTaskName.fill(taskName)
-    //await inputTaskName.press('Enter') Se quizesse cadastrar a tarefa usando a tecla Enter
-    //await page.click('xpath=//button[contains(text(), "Create")]') // outra estrategia com xpath
+
     await page.click('css=button >> text=Create')
 
     const target = page.locator(`css=.task-item p >> text=${taskName}`)
     await expect(target).toBeVisible()
+})
 
+test.only('nao deve permitir tarefa duplicada', async ({ page, request }) => {
+
+    const task = {
+        name: 'Criar artigo no Medium',
+        is_done: false
+    }
+
+    await request.delete('http://localhost:3333/helper/tasks/' + task.name)
+
+    const newTask = await request.post('http://localhost:3333/tasks', { data: task })
+    expect(newTask.ok()).toBeTruthy()
+    
+
+    await page.goto('http://localhost:8080')
+
+    const inputTaskName = page.locator('input[class*=InputNewTask]')
+    await inputTaskName.fill(task.name)
+
+    await page.click('css=button >> text=Create')
+
+    const target = page.locator('.swal2-html-container')
+    await expect(target).toHaveText('Task already exists!')
 })
 
