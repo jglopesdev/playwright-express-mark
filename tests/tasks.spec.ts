b57af422b195650216c273fb5db519a5bfeb8f46
Joao Lopes <jgsantanalopes@gmail.com>
2025-03-21 09:38:28 -0300

    test: should not register when the field is not filled

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import { TaskModel } from './fixtures/task.model'
 import { deleteTaskByHelper, postTask } from './support/helpers'
 import { TasksPage } from './support/pages/tasks'
@@ -28,9 +28,23 @@ test('nao deve permitir tarefa duplicada', async ({ page, request }) => {
     await postTask(request, task)
 
     const tasksPage: TasksPage = new TasksPage(page)
-    
+
     await tasksPage.go()
     await tasksPage.create(task)
     await tasksPage.alertHaveText('Task already exists!')
 })
 
+test('campo obrigatorio', async ({ page }) => {
+    const task: TaskModel = {
+        name: '',
+        is_done: false
+    }
+
+    const tasksPage: TasksPage = new TasksPage(page)
+
+    await tasksPage.go()
+    await tasksPage.create(task)
+
+    const validationMessage = await tasksPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
+    expect(validationMessage).toEqual('This is a required field')
+})
